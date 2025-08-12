1d5f6b0a2aab094ff7f33aba78d69f149920620b
keyner barrios <keinerba.ochoa@gmail.com>
2025-08-12 15:48:22 -0500

    feat:creating the folder structure

---
+++ backend/db.js
@@ -0,0 +1,28 @@
+//  Importamos el paquete 'mysql2' para poder hablar con MySQL
+import mysql from 'mysql2'
+
+//  Creamos la conexión a la base de datos Clever Cloud
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Qwe.123*',
+    database: 'pd_camilo_parra_ciegana',
+    port: 3306,
+    connectionLimit: 10,
+    multipleStatements: true,
+    queueLimit: 0
+})
+
+
+// Verificamos si la conexión funciona correctamente
+db.getConnection((error, connection) => {
+    if (error) {
+        console.error(' Error al conectar con la base de datos:', error.message);
+    } else {
+        console.log(' Conexión a la base de datos exitosa');
+        connection.release(); // Muy importante: liberar la conexión después de usarla
+    }
+})
+
+//  Exportamos la conexión para poder usarla en los demás archivos
+export default db;
---
+++ backend/index.js
@@ -0,0 +1,35 @@
+// Importamos express (el servidor web que vamos a usar para las rutas)
+import express from 'express'
+
+// Importamos cors para que el frontend (por ejemplo, Vite) pueda comunicarse con el backend sin problemas
+import cors from 'cors'
+
+// Importamos los archivos que tienen las rutas de cada tabla
+import customerRoutes from './routes/customes.js'
+import invoicesRoutes from './routes/invoices.js'
+import paymentRoutes from './routes/payment_platforms.js'
+import transationRoutes from './routes/transaction.js'
+import statusRouter from './routes/transation_status.js'
+
+// Creamos la aplicación de Express (esto es como el servidor en sí)
+const app = express()
+
+// Usamos cors para permitir peticiones desde otros orígenes (como Vite en localhost:5173)
+app.use(cors())
+
+// Le decimos al servidor que entienda datos en formato JSON (para recibir datos del Excel en el frontend)
+app.use(express.json())
+
+// Configuramos las rutas para que cuando alguien acceda a /api/usuarios, se use el archivo correspondiente
+
+
+app.use('/api/customer', customerRoutes)
+app.use('/api/invoices', invoicesRoutes)
+app.use('/api/payment', paymentRoutes)
+app.use('/api/transation', transationRoutes)
+app.use('/api/status',statusRouter)
+
+// Encendemos el servidor en el puerto 3000. Cuando esté listo, muestra un mensaje en la consola.
+app.listen(3000, () => {
+    console.log('Servidor backend encendido en http://localhost:3000')
+})
